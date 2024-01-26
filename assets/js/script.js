c55d18fe7f043efb0d9e2e69afee5ecc7625c0d8
Taylor carr <taylormichaelcarr@yahoo.com>
2024-01-26 14:05:03 +0000

    added even listener for submit button, random number function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,29 @@
+// wait for the dom to finish loading before running the game
+// get the button elelments and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+           if (this.getAttribute("data-type") === "submit") {
+               alert("You clicked Submit!");
+           } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+           }
+        });
+    }
+});
+
+
 function runGame() {
 
+    // creates two random numbers batween 1 and 25
+      let num1 = Math.floor(Math.random() * 25) + 1;
+      let num2 = Math.floor(Math.random() * 25) + 1;   
 }
+
 
 function checkAnswer() {
 
@@ -19,7 +42,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionalQuestion() {
+function displayAdditionQuestion() {
 
 }
 
