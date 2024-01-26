35354911f28349ce6412fd885dfe33f0fecedb19
Taylor carr <taylormichaelcarr@yahoo.com>
2024-01-26 14:44:15 +0000

    added alert message, addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,18 +10,27 @@ document.addEventListener("DOMContentLoaded", function() {
                alert("You clicked Submit!");
            } else {
             let gameType = this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`);
+            runGame(gameType);
            }
         });
     }
+
+    runGame("addition");
 });
 
 
-function runGame() {
+function runGame(gameType) {
 
     // creates two random numbers batween 1 and 25
       let num1 = Math.floor(Math.random() * 25) + 1;
-      let num2 = Math.floor(Math.random() * 25) + 1;   
+      let num2 = Math.floor(Math.random() * 25) + 1;
+      
+      if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);     
+     } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+     }
 }
 
 
@@ -42,8 +51,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;
+        document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
